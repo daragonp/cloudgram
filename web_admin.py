559c169b7bec98929d986fc4b4fcc25e1e96ae72
Dilinger Aragon Perea <daragonp@outlook.com>
2026-03-13 13:24:26 -0500

    Cambios previos a despliegue

--- web_admin.py
+++ web_admin.py
@@ -433,5 +433,5 @@ def archivos_errores():
         return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5050))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
