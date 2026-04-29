3bddd7887623e219b9dd8b7f6da6e61ecca46e4b
Dilinger Aragon Perea <daragonp@outlook.com>
2026-04-29 17:36:02 -0500

    Login limiter with flask limiter

--- web_admin.py
+++ web_admin.py
@@ -42,7 +42,6 @@ from flask_limiter.util import get_remote_address
 limiter = Limiter(
     get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"],
     storage_uri="memory://"
 )
 
